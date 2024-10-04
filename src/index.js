a1da989846ba401ff7f0b4d0450b7ecb75f38eaa
Aniket Sahoo <aniket.sahoo379@gmail.com>
2024-10-04 18:06:03 +0530

    useRef for uncontrolled components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+
+function App() {
+  const sound = useRef();
+  const color = useRef();
+
+  const submit = (e) => {
+    e.preventDefault();
+    const soundVal = sound.current.value;
+    const colorVal = color.current.value;
+    alert(`Sound: ${soundVal}, Color: ${colorVal}`);
+    console.log(soundVal, colorVal);
+    sound.current.value = '';
+    color.current.value = '';
+  }
+
+  return (
+    <form onSubmit={submit}>
+      <input ref={sound} type="text" placeholder="Sound..."/>
+      <input ref={color} type="color"/>
+      <button>ADD</button>
+    </form>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +34,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
