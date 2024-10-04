dc34bde88da6a7c504ede29dbcd8904723ac4b56
Aniket Sahoo <aniket.sahoo379@gmail.com>
2024-10-04 11:23:01 +0530

    useState Hook practice creating StarRating Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { FaStar } from "react-icons/fa";
 import reportWebVitals from './reportWebVitals';
+
+const createArray = (length) => [...Array(length)]
+
+function Star({ selected = false, onSelect }) {
+  return <FaStar color={selected ? "blue" : "grey"} onClick={onSelect} />
+}
+
+function StarRating({ totalStars=5 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star 
+        key={i} 
+        onSelect={() => setSelectedStars(i + 1)}
+        selected={selectedStars > i} 
+        />
+      ))}
+      <p>Number of selected Stars: {selectedStars}</p>
+    </>
+  );
+}
+
+function App({name, age}) {
+  return (
+    <StarRating totalStars={10}/>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App name="Sahoo" age="23"/>
   </React.StrictMode>
 );
 
