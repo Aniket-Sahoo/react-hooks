19a28b8eb4be25884e44a0b653932386e2ef6b0b
Aniket Sahoo <aniket.sahoo379@gmail.com>
2024-10-04 11:47:03 +0530

    useEffect Practice - fetching data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+function App() {
+  const [data, setData] = useState([]); 
+
+  useEffect(() => {
+    fetch('https:api.github.com/users')
+      .then(response => response.json())
+      .then(json => setData(json));
+  }, []);
+
+  if(data){
+    return (
+      <>
+        <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.login}</li>))}
+        </ul>
+        <button button onClick={() => setData([])}>Clear</button>
+      </>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +32,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
